7c7ea30ecc278d491d28fcf92c915d2257758cd5
MrSpejn <tomek.josiak@gmail.com>
2017-12-07 19:51:18 +0100

    Keep card ratio

--- index.py
+++ index.py
@@ -2,27 +2,72 @@ from matplotlib import pyplot
 import cv2
 import numpy
 
+# cards = [
+#     './cards/cards1.png',
+#     './cards/cards2.jpg',
+#     './cards/cards3.jpg',
+#     './cards/cards4.jpg',
+#     './cards/cards5.jpg',
+#     './cards/cards6.jpg',
+#     './cards/cards7.jpg',
+#     './cards/cards8.jpg',
+#     './cards/cards9.jpg',
+#     './cards/cards10.jpg',
+# ]
 cards = [
-    './cards/cards1.png',
-    './cards/cards2.jpg',
-    './cards/cards3.jpg',
-    './cards/cards4.jpg',
-    './cards/cards5.jpg',
-    './cards/cards6.jpg',
-    './cards/cards7.jpg',
-    './cards/cards8.jpg',
-    './cards/cards9.jpg',
-    './cards/cards10.jpg',
+    './cardsv2/cards1.png',
+    './cardsv2/cards2.jpg',
+    './cardsv2/card3.jpg',
+    './cardsv2/card4.jpg',
+    './cardsv2/card5.JPG',
+    './cardsv2/card6.JPG',
+    './cardsv2/card7.JPG',
+    './cardsv2/card8.JPG',
+    './cardsv2/card9.JPG',
+    './cardsv2/card11.JPG',
+    './cardsv2/card12.JPG',
+    './cardsv2/card13.JPG',
+    './cardsv2/card14.JPG',
+    './cardsv2/card15.JPG',
 ]
+
 def order_points(points):
     rect = numpy.zeros((4, 2), dtype = "float32")
+    rect2 = numpy.zeros((4, 2), dtype = "float32")
     s = points.sum(axis = 2)
     rect[2] = points[numpy.argmax(s)]
     rect[0] = points[numpy.argmin(s)]
     diff = numpy.diff(points, axis = 2)
     rect[1] = points[numpy.argmin(diff)]
     rect[3] = points[numpy.argmax(diff)]
-    return rect
+
+    x,y,w,h = cv2.boundingRect(rect)
+
+    if (h > 0.9*w): return rect
+    else:
+        rect2[0] = rect[3]
+        rect2[1] = rect[0]
+        rect2[2] = rect[1]
+        rect2[3] = rect[2]
+        return rect2
+
+def extract_rect(image, contours, idx):
+    epsilon = 0.01*cv2.arcLength(contours[idx], True)
+    approx = cv2.approxPolyDP(contours[idx], epsilon, True)
+    approx = order_points(approx)
+    approx = approx.astype(numpy.float32)
+
+    h = numpy.array([[0,0],[749,0],[749,999],[0,999]], numpy.float32)
+    transform = cv2.getPerspectiveTransform(approx, h)
+
+    approx = approx.astype(numpy.int32)
+    mask = numpy.zeros_like(image)
+    cv2.drawContours(mask, contours, idx, [255, 255, 255], -1)
+    out = numpy.zeros_like(image)
+    out[mask == 255] = image[mask == 255]
+    wrap = cv2.warpPerspective(out, transform, (750, 1000))
+    return wrap
+
 
 def main(): 
     fig = pyplot.figure(figsize=(40, 40))
@@ -34,7 +79,7 @@ def main():
         gray = cv2.cvtColor(card, cv2.COLOR_BGR2GRAY)
         blur = cv2.GaussianBlur(gray, (1,1), 1000)
         mean = numpy.mean(card)
-        flag, thresh = cv2.threshold(blur, 1.6*mean, 255, cv2.THRESH_BINARY)
+        flag, thresh = cv2.threshold(blur, 1.45*mean, 255, cv2.THRESH_BINARY)
 
         _, contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         max_area = max(map(lambda c: cv2.contourArea(c), contours))
@@ -42,28 +87,14 @@ def main():
         
       
 
-        picture = fig.add_subplot(11, 5, 5*idx + 1)
+        picture = fig.add_subplot(16, 5, 5*idx + 1)
         picture.axis('off')
         pyplot.imshow(cv2.cvtColor(card, cv2.COLOR_BGR2RGB))
         for ci in range(len(contours)):
-            epsilon = 0.01*cv2.arcLength(contours[ci], True)
-            approx = cv2.approxPolyDP(contours[ci], epsilon, True)
-            approx = order_points(approx)
-            approx = approx.astype(numpy.float32)
-
-            h = numpy.array([[0,0],[999,0],[999,999],[0,999]], numpy.float32)
-            transform = cv2.getPerspectiveTransform(approx, h)
-
-            approx = approx.astype(numpy.int32)
-            mask = numpy.zeros_like(card)
-            cv2.drawContours(mask, contours, ci, [255, 255, 255], -1)
-            out = numpy.zeros_like(card)
-            out[mask == 255] = card[mask == 255]
-            wrap = cv2.warpPerspective(out, transform, (1000, 1000))
-
-            picture = fig.add_subplot(11, 5, 5*idx + 2 + ci)
+            single_card_rect = extract_rect(card, contours, ci)
+            picture = fig.add_subplot(16, 5, 5*idx + 2 + ci)
             picture.axis('off')
-            pyplot.imshow(cv2.cvtColor(wrap, cv2.COLOR_BGR2RGB))
+            pyplot.imshow(cv2.cvtColor(single_card_rect, cv2.COLOR_BGR2RGB))
 
     fig.savefig("result.pdf")
 
